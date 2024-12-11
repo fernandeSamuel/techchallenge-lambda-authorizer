44a540dc53aa5a6e16fb7469ff1a20ab759cfb71
SamuelOFernandes <samuel.o.fernandes2020@gmail.com>
2024-12-10 23:10:50 -0300

    add gateway

--- main.tf
+++ main.tf
@@ -2,14 +2,60 @@ provider "aws" {
   region = "us-east-2" # Ajuste para a região correta
 }
 
+resource "aws_api_gateway_rest_api" "example" {
+  name        = "API Gateway Test"
+  description = "API Gateway created from OpenAPI spec"
+}
+
+resource "aws_api_gateway_resource" "cpf_auth" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  parent_id   = aws_api_gateway_rest_api.example.root_resource_id
+  path_part   = "CPFAuth"
+}
+
+resource "aws_api_gateway_method" "post_method" {
+  rest_api_id   = aws_api_gateway_rest_api.example.id
+  resource_id   = aws_api_gateway_resource.cpf_auth.id
+  http_method   = "POST"
+  authorization = "CUSTOM"
+  authorizer_id = aws_api_gateway_authorizer.lambda_authorizer.id
+}
+
+resource "aws_api_gateway_authorizer" "lambda_authorizer" {
+  name                   = "lambdaAuthorizer"
+  rest_api_id            = aws_api_gateway_rest_api.example.id
+  type                   = "REQUEST"
+  authorizer_uri         = "arn:aws:apigateway:us-east-2:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-2:148761639942:function:CPFAuth/invocations"
+  identity_source        = "method.request.header.Authorization"
+  authorizer_result_ttl_in_seconds = 0
+}
+
+resource "aws_api_gateway_integration" "post_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.example.id
+  resource_id             = aws_api_gateway_resource.cpf_auth.id
+  http_method             = aws_api_gateway_method.post_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = "arn:aws:apigateway:us-east-2:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-2:148761639942:function:CPFAuth/invocations"
+}
+
+resource "aws_api_gateway_deployment" "example" {
+  rest_api_id = aws_api_gateway_rest_api.example.id
+  stage_name  = "default"
+
+  depends_on = [aws_api_gateway_integration.post_integration]
+}
+
+
 # S3 Bucket para armazenar o código Lambda
 resource "aws_s3_bucket" "lambda_bucket" {
-  bucket = "lambda-code-bucket-${random_string.suffix.result}"
+  bucket = "lambda-code-bucket-cpf-validator-${random_string.suffix.result}"
   acl    = "private"
 
   tags = {
     Name = "LambdaCodeBucket"
   }
+
 }
 
 resource "random_string" "suffix" {
@@ -32,6 +78,7 @@ resource "aws_iam_role" "lambda_role" {
       }
     ]
   })
+  
 }
 
 # Attach AWS Managed Policy for Lambda Execution
@@ -55,4 +102,5 @@ resource "aws_lambda_function" "cpf_validator" {
   tags = {
     Name = "CPFValidatorLambda"
   }
+  depends_on = [ aws_api_gateway_rest_api.example,aws_iam_role.lambda_role, aws_s3_bucket.lambda_bucket, aws_iam_role_policy_attachment.lambda_execution_policy ]
 }
